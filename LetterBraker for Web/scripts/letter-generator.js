983165aee98ef15edcbce51f3e0dbfc3c43ca0fd
Nikolay <nikecha96@gmail.com>
2014-02-28 15:01:16 +0200

    Correct rounding fix
    Fixed an issue of numbers being rounded to their intergrer value (5.72
    being rounded to 5 for example)

--- LetterBraker for Web/scripts/letter-generator.js
+++ LetterBraker for Web/scripts/letter-generator.js
@@ -1,9 +1,21 @@
 (function(){
 	
+	function correctRounding(number){
+		//
+		number *= 10;
+		if ( number % 10 < 5 ){
+			return number /10 | 0;
+		}
+		else return number/10 + 1 | 0;
+	}
+
 	function generateRandomKeyCode(){
 		// a = 65, z = 90
-		return Math.floor(Math.random()*25 + 65);
+		//Error Check -- alert(correctRounding(5.72));
+
+		return correctRounding(Math.floor(Math.random()*25 + 65));
 	}
+
 	LG = {
 		keyCode : generateRandomKeyCode(),
 		refreshCount : 0,
